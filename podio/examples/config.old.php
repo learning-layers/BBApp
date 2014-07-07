f61b3f4bd620528a09ceed0a6b9ebb1bf86e32f1
owengray <owzone1@gmail.com>
2014-07-07 11:17:38 +0200

    Preporation for new phonegap file layout

--- podio/examples/config.old.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("CLIENT_ID", "learning-toolbox");
-define("CLIENT_SECRET", "pzGca3nRRYjUvwaUQBK8Czvj4ZiPshSyAV4yKrkUA8PXgqoaQig2BswZ4GAjc0DK");
-define("USERNAME", "");
-define("PASSWORD", "");
-
-// Make sure errors are output to the screen
-ini_set('display_errors', '1');
